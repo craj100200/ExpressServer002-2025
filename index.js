1c2be31ceef650315b175f26f6079369177adb67
RajeshC <craj100200@gmail.com>
2025-03-19 22:56:38 +0400

    first push

---
+++ index.js
@@ -0,0 +1,11 @@
+import  express  from "express";
+
+const app= express();
+
+app.get('/' , (req,res)=> {res.send("Hello"); });
+
+app.get('/ping' , (req, res)=> {res.send("pong");});
+
+const port = 4000;
+
+app.listen(port);
