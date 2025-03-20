892859984a1582f731cf66cd44f89990a4475fa8
craj100200 <122225396+craj100200@users.noreply.github.com>
2025-03-20 22:48:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+
+const router = express.Router();
+
 // CREATE Student
 router.post('/students', async (req, res, next) => {
 	try 
