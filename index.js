1510b9ef628b76c0ca029cdca014e9c56781fe49
craj100200 <122225396+craj100200@users.noreply.github.com>
2025-03-20 22:34:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express  = require("express");
-
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app= express();
 
 
-const  mongoose  = require("mongoose");
+
 const Schema = mongoose.Schema;
 
 let studentSchema = new Schema({
