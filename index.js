a0c9e38072df43c124ed9e84eac050d0236fe349
craj100200 <122225396+craj100200@users.noreply.github.com>
2025-03-20 22:29:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ const express  = require("express");
 
 const app= express();
 
+
+const  mongoose  = require("mongoose");
+const Schema = mongoose.Schema;
+
+let studentSchema = new Schema({
+    id: {
+	    type:Number
+    }, 
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    rollNo: {
+        type: Number
+    },
+    dateOfBirth: {
+	type: String
+    }
+}, {
+    collection: 'students'
+});
+
+
+
+
+
+
 app.get('/' , (req,res)=> {res.send("Hello"); });
 
 app.get('/ping' , (req, res)=> {res.send("pong");});
