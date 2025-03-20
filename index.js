66eac052d23af36373cb09678a26818740be273e
craj100200 <122225396+craj100200@users.noreply.github.com>
2025-03-20 22:39:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ let studentSchema = new Schema({
 // Load environment variables from .env file
 //const { MONGODB_URI, PORT } = process.env;
 const MONGODB_URI = '';
-const PORT = 3000;
+
 
 // Connecting to MongoDB Database
 mongoose.connect('mongodb+srv://mydbuser:mydbuser@cluster0.hfu9n.mongodb.net/Student?retryWrites=true&w=majority&appName=Cluster0', {
@@ -49,12 +49,133 @@ mongoose.connect('mongodb+srv://mydbuser:mydbuser@cluster0.hfu9n.mongodb.net/Stu
     });
 
 // Initialize Express app
-const app = express();
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParsconst router = express.Router();
+
+// CREATE Student
+router.post('/students', async (req, res, next) => {
+	try 
+	{
+		const data = await Student.create(req.body);
+        	console.log(data);
+        	res.json(data);
+	} 
+	catch (error) 
+	{
+        	return next(error);
+    	}
+});
+
+// READ Students
+router.get('/', async (req, res, next) => {
+	try 
+	{
+        	const data = await Student.find();
+        	res.json(data);
+    	} 
+	catch (error) 
+	{
+        	return next(error);
+    	}
+});
+
+// UPDATE Student
+router.route('/students/:id')
+	// Get Single Student
+	.get(async (req, res, next) => {
+        try 
+	{
+        	const data = await Student.findById(req.params.id);
+            	res.json(data);
+        } 
+	catch (error) 
+	{
+            	return next(error);
+        }
+    })
+    // Update Student Data
+	.put(async (req, res, next) => {
+	   	try 
+		{ 
+			// Find the Student by id and update
+			const updatedStudent = await Student.findOneAndUpdate(
+      				{ id: req.params.id }, // Query criteria
+      				req.body,        // Update data
+      				{ new: true }       // Options: return the updated document
+    				);
+
+    			res.json(updatedStudent);
+    			if (updatedStudent) 
+			{
+			      console.log('Student updated successfully:', updatedStudent);
+			} 
+			else 
+			{
+	      		console.log('Student not found');
+    			}
+	    	} 
+	    	catch(error) 
+		{ 
+			return next(error);
+	    	} 
+
+     	//   try {
+            //const data = await Student.findByIdAndUpdate(req.params.id, {
+       //     const data = await Student.find( {id : req.params.id }, {
+       //         $set: req.body,
+       //     }, { new: true });
+       //     res.json(data);
+       //     console.log("Student updated successfully!");
+       // } catch (error) {
+	//	console.log("Error "); console.log(error);
+        //    return next(error);
+        //}
+    });
+
+// DELETE Student
+router.delete('/students/:id', async (req, res, next) => {
+
+try 
+{ 
+	const deletedStudent = await Student.findOneAndDelete(
+     		{ id: req.params.id }// Query criteria
+    	);
+
+
+    	if (deletedStudent) 
+	{
+    		res.status(200).json(deletedStudent);
+      		console.log('Student deleted successfully:', deletedStudent);
+    	} 
+	else 
+	{
+		res.status(404).send("Student not found");
+      		console.log('Student not found');
+    	} 
+} 
+catch(error) 
+{
+	console.log("Error deleting Student record");console.log(error);
+	return next(error);
+} 
+
+    //try {
+      //  const data = await Student.findByIdAndRemove(req.params.id);
+        //res.status(200).json({
+          //  msg: data,
+       // });
+   // } catch (error) {
+     //   return next(error);
+   // }
+});
+er.urlencoded({ extended: true }));
 app.use(cors());
 
+//student.router.js
+//------------------------------------------------------------------------
 
+//------------------------------------------------------------------------
 
 
 
